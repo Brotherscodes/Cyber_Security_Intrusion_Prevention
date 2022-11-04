9643a22c28b159c8f4d948e61d1451a7373150ee
sktwelve <106925333+sktwelve@users.noreply.github.com>
2022-11-03 21:03:43 -0400

    code updated using cleaned csv

--- SQL_Database_Pedro/databasetransfer.py
+++ SQL_Database_Pedro/databasetransfer.py
@@ -2,15 +2,16 @@
 from sqlalchemy import create_engine
 from config import db_password
 import time
+import pandas as pd
 
 db_string = f"postgresql://postgres:{db_password}@127.0.0.1:5432/csidb"
 
 #import database from csv
-
+data = pd.read_csv("Resources/Cleaned_DDos_DataFrame.csv")
+print(data)
 
 # Create connection to database
 engine = create_engine(db_string)
-
 
 # Transfer data from dataframes to SQL Database
 df.to_sql(name='flows', con=engine, if_exists='replace')
---
+++ SQL_Database_Pedro/datajoins.sql
@@ -0,0 +1,33 @@
+SELECT fl."Flow ID",
+	fl." Source IP",
+    fl." Destination Port",
+	fl." Destination IP",
+	pa." Average Packet Size",
+	pa." Bwd Packet Length Std"
+INTO flow_summary
+FROM flows as fl
+LEFT JOIN packets as pa
+ON fl."Flow ID" = pa."Flow ID";
+
+SELECT fl."Flow ID",
+	fl." Source IP",
+	fl." Destination IP",
+	ti." Flow IAT Std",
+	ti." Flow Duration",
+	ti." Timestamp"
+INTO flow_time
+FROM flows as fl
+LEFT JOIN time as ti
+ON fl." Flow ID" = ti." Flow_ID";
+
+SELECT ti." Timestamp"
+	pa." Average Packet Size",
+	pa." Bwd Packet Length Std"
+	ti." Flow IAT Std",
+	ti." Flow Duration"
+INTO pac_time
+FROM packets as pa
+LEFT JOIN time as ti
+ON pa.times = ti.times;
+
+
